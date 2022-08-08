a01da7dce2880c92ff9de9a51aca027ae303b425
mibek <mibek@gmail.com>
2022-08-08 19:13:14 +0545

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 
-import { useState } from 'react';
+
 import './App.css';
 import Navbar from './components/Navbar';
-// import About from './components/About';
+//  import About from './components/About';
 import TextForm from './components/TextForm';
+import React,{useState} from 'react'
 function App() {
-  const [mode,setMode] = useState('light');//wheter dark mode is enable or nont
+  const [mode, setMode] = useState('dark');//wheter dark mode is enable or nont
   return (
     <>
        <Navbar title="TextTile" aboutText="AboutUs" mode={mode} />
        <div className="container my-3">
        <TextForm heading="Enter the text to analyze below"/> 
-       
+   
        </div>
        
     </>
